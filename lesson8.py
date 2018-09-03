e4ba7945de7723c4d7fb3b3368bfb7fddba43b8c
vivi <1135504471@qq.com>
2018-09-03 17:33:15 +0800

    hi,new repository

---
+++ lesson8.py
@@ -0,0 +1,23 @@
+# coding:cp936
+
+import os
+def search(key,address):
+    file_list = []
+    for path,dirnames,filenames in os.walk(address):
+        print('search'+path+'...')
+        for name in filenames:
+            if key in name:
+                file_list.append(path+'\\'+ name)
+            '''
+            with open(path+'\\'+name)as f :
+                for l in f :
+                    if key in l :
+                        file_list.append(path+'\\'+ name+':'+l)\\\
+                        '''
+    return file_list
+kw = input('search:')
+ad = input('in:')
+if not ad.strip():
+    ad = '.'
+for i in search(kw,ad):
+    print(i)
